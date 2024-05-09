9a2036e7878bcc139cf32c0e2618e244dc7daec5
Johnson Mwebaze <151113583+jmwebazeutamu@users.noreply.github.com>
2024-05-09 07:26:48 +0200

    Update personalized_greeting.py

--- personalized_greeting.py
+++ personalized_greeting.py
@@ -1,12 +1,14 @@
 # personalized_greeting.py
 
-def greet():
-    # Create a variable called myname and assign your name to it
+def greet(myname):
     
-    # modify Print statment below to print a personalized greeting e.g Hello John, welcome to computer programming
-
+    # modify the print statement below to print a personalised greeting the output should look like Hello Mike, welcome to computer programming
+    
     print(f"Hello myname, welcome to computer programming.")
 
-# This check ensures the code only executes when run as the main script, not when imported
 if __name__ == "__main__":
-    greet()
+    
+    # create a variable here called myname
+    #Assign the variable myname with your actual name as a string. For example, if your name is John, you would write: myname = "John".
+    
+    greet(myname)
